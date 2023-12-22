32f2e6049aea618d4d5583c0be0248e0ebd240c8
Harvey Wang <wangzhemh@outlook.com>
2023-12-23 01:02:57 +0800

    update the launch.json file

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,14 @@
-let str: string = Date().toString();
-console.log("Hello World");
+let a = [];
+const names = ["Alice", "Bob", "Eve"];
+
+
+for (let i = 0; i < 10; i++) {
+    a.push(i);
+    console.log(i);
+    
+}
+console.log(names);
+
+let b = a.filter(x => x > 2);
+console.log("done.");
+console.log("end");
